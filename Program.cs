e7b7ffdb95c65f84d3047eb095c71fd082f1d009
Juan E Afable <juanjuanzerojuan@gmail.com>
2021-08-20 16:38:34 -0400

    updated files for deployment

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Statiq.App;
 using Statiq.Web;
+using Statiq.Common;
 
 namespace MySite
 {
   public class Program
   {
-    public static async Task<int> Main(string[] args) =>
-      await Bootstrapper
-        .Factory
-        .CreateWeb(args)
-        .RunAsync();
+    public static async Task<int> Main(string[] args) 
+    {
+
+      return await Bootstrapper
+              .Factory
+              .CreateWeb(args)
+              .RunAsync();
+    }
+      
   }
 }
